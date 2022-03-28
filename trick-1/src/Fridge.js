0393df4123b5856e8a91bf73f7afc1552046d723
Nathan <NFoxley@users.noreply.github.com>
2022-03-28 15:15:50 -0500

    Removed unused Fridge

--- trick-1/src/Fridge.js
+++ trick-1/src/Fridge.js
@@ -1,29 +1,29 @@
 function displayFridge() {
-    class Fridge {
-        constructor(
-            name,
-            temperature,
-            volume,
-            doors,
-            food,
-            drink,
-            freezerLocation,
-            doorOpen
-        ) {
-            this.name = name;
-            this.temperature = temperature;
-            this.volume = volume;
-            this.doors = doors;
-            this.food = food;
-            this.drink = drink;
-            this.freezerLocation = freezerLocation;
-            this.doorOpen = doorOpen;
-        }
-        openDoor(doorStatus) {
-            this.doorOpen = doorStatus;
-        }
+    // class Fridge {
+    //     constructor(
+    //         name,
+    //         temperature,
+    //         volume,
+    //         doors,
+    //         food,
+    //         drink,
+    //         freezerLocation,
+    //         doorOpen
+    //     ) {
+    //         this.name = name;
+    //         this.temperature = temperature;
+    //         this.volume = volume;
+    //         this.doors = doors;
+    //         this.food = food;
+    //         this.drink = drink;
+    //         this.freezerLocation = freezerLocation;
+    //         this.doorOpen = doorOpen;
+    //     }
+    //     openDoor(doorStatus) {
+    //         this.doorOpen = doorStatus;
+    //     }
         
-    }
+    // }
     
     return (
         <>
