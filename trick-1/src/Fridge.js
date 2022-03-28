ec9e4f862bfa70418bb05fe1e9736a53d04ccda8
Nathan <NFoxley@users.noreply.github.com>
2022-03-28 15:27:05 -0500

    Add fridge name

--- trick-1/src/Fridge.js
+++ trick-1/src/Fridge.js
@@ -1,33 +1,36 @@
 function displayFridge() {
-    // class Fridge {
-    //     constructor(
-    //         name,
-    //         temperature,
-    //         volume,
-    //         doors,
-    //         food,
-    //         drink,
-    //         freezerLocation,
-    //         doorOpen
-    //     ) {
-    //         this.name = name;
-    //         this.temperature = temperature;
-    //         this.volume = volume;
-    //         this.doors = doors;
-    //         this.food = food;
-    //         this.drink = drink;
-    //         this.freezerLocation = freezerLocation;
-    //         this.doorOpen = doorOpen;
-    //     }
-    //     openDoor(doorStatus) {
-    //         this.doorOpen = doorStatus;
-    //     }
+    class Fridge {
+        constructor(
+            name,
+            temperature,
+            volume,
+            doors,
+            food,
+            drink,
+            freezerLocation,
+            doorOpen
+        ) {
+            this.name = name;
+            this.temperature = temperature;
+            this.volume = volume;
+            this.doors = doors;
+            this.food = food;
+            this.drink = drink;
+            this.freezerLocation = freezerLocation;
+            this.doorOpen = doorOpen;
+        }
+        openDoor(doorStatus) {
+            this.doorOpen = doorStatus;
+        }
         
-    // }
+    }
     
     return (
         <>
             <div>My Fridge 1</div>
+            <ul>
+                <li>Name: {Fridge.name}</li>
+            </ul>
         </>
     )
     
